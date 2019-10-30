c28b42d5dbf3ab8b50b34f2b93834bc17c676304
ozwaldo <ozwaldo@gmail.com>
2019-10-30 13:48:30 -0600

    Inicio

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    
+    <title>Blog de Tecnología</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <div class="row d-flex align-items-center">
+                <div class="col-4"></div>
+                <div class="col-4 text-center">
+                    <a href="#" class="blog-logo">
+                        Blog
+                    </a>
+                </div>
+                <div class="col-4"></div>
+            </div>
+        </header>
+        <div class="nav-blog">
+            <nav class="nav d-flex justify-content-between">
+                <a href="#" class="p-2 text-muted">Inicio</a>
+                <a href="#" class="p-2 text-muted">Categoria 1</a>
+                <a href="#" class="p-2 text-muted">Categoria 2</a>
+                <a href="#" class="p-2 text-muted">Categoria 3</a>
+            </nav>
+        </div>
+        <div class="jumbotron text-white">
+            <div class="col-10">
+                <h1 class="display-4">
+                    Título
+                </h1>
+                <p class="my-3">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing
+                    elit. Beatae harum adipisci numquam fugiat.
+                    Quod recusandae fugiat obcaecati pariatur.
+                    Illo debitis, ad obcaecati veritatis aliquam
+                    consequatur reprehenderit quae officiis optio dolore!
+                </p>
+                <p>
+                    <a href="#" class="text-white">Leer más</a>
+                </p>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-md-6">
+                <div class="card text-white">
+                    <img src="img/bg-card-1.jpg" class="card-img-top" alt="Card 1">
+                    <div class="card-img-overlay d-flex align-items-end">
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-info">
+                                Categoría
+                            </strong>
+                            <h3 class="card-title">Título</h3>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius Lorem ipsum dolor sit,
+                                amet consectetur.
+                            </p>
+                            <a href="#">Leer más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card text-white">
+                    <img src="img/bg-card-2.jpg" class="card-img-top" alt="Card 1">
+                    <div class="card-img-overlay d-flex align-items-end">
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-info">
+                                Categoría
+                            </strong>
+                            <h3 class="card-title">Título</h3>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius Lorem ipsum dolor sit,
+                                amet consectetur.
+                            </p>
+                            <a href="#">Leer más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div> <!-- /container -->
+    <footer class="blog-footer mt-5">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-4">
+                    <div class="footer-copyright text-center py-3">
+                        Todos los derechos reservados - 2019
+                    </div>
+                </div>
+                <div class="col-4">
+                    <ul class="redes-sociales social-circle">
+                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+                        <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
